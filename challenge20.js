3373e3be656beeeebe9d46369afad2f6dc06d982
Alex MacLure <almaclure@gmail.com>
2021-12-14 17:35:23 -0500

    Complete challenge 20

--- challenge20.js
+++ challenge20.js
@@ -1,3 +1,31 @@
 const confirmReentryPlans = (speed, missionData, checks) => {
-  // Code here!
+  let boolReturn = true;
+  if (speed > checks.maxSpeed || speed < checks.minSpeed) {
+    boolReturn = false;
+  }
+  Object.keys(checks.dataEntries).forEach((type) => {
+    if (missionData[type].length !== checks.dataEntries[type]){
+      boolReturn = false;
+    }
+  });
+  return boolReturn;
+};
+
+const speed = 40
+const missionData = {
+  astro:["...","..."], 
+  bio:["..."], 
+  physics:["..."]
 }
+
+const checks = {
+  maxSpeed:50,
+  minSpeed:20,
+  dataEntries:{
+    astro:3,
+    bio:1,
+    physics:1
+  }
+}
+  
+console.log(confirmReentryPlans(speed, missionData, checks));
